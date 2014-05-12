41f84286903e72dc25b8f691afc66d2b286ff8c4
basile.duplessis <basile.duplessis@gmail.com>
2014-05-12 17:02:49 +0200

    test for ch02 Ex10

--- src/main/scala/com/basile/scala/ch02/Ex10.scala
+++ src/main/scala/com/basile/scala/ch02/Ex10.scala
@@ -1,19 +1,31 @@
 package com.basile.scala.ch02
 
+import scala.math.pow
+
 /**
  * Write a function that computes x^n , where n is an integer.
  * Use the following recursive definition:
  * • x^n = y^2 if n is even and positive, where y = x^n / 2 .
  * • x^n = x· x^n – 1 if n is odd and positive.
  * • x^0 = 1.
- * • x^n = 1 / x – n if n is negative.
+ * • x^n = 1 / x^n if n is negative.
  *
  * Don’t use a return statement.
  */
 object Ex10 extends App {
 
   def compute(x: Double, n: Int): Double = {
-    //1.2
+    if (n > 0 && n % 2 == 0) pow( pow( x, n / 2 ), 2 )
+    else if (n > 0 && n % 2 != 0) x * pow( x, n - 1 )
+    else if (n < 0) 1 / pow( x, -n )
+    else 1
   }
+
+  assert(compute(5, 0) == 1)
+  assert(compute(5, 2) == 25)
+  assert(compute(5, 3) == 125)
+  assert(compute(5, -3) == .008)
+
+
 
 }
