683dd04d95578d4328d4449aa700a71f1598c968
basile.duplessis <basile.duplessis@gmail.com>
2014-05-12 14:56:46 +0200

    Scala for impatient Ch2

---
+++ src/main/scala/com/basile/scala/ch02/Ex10.scala
@@ -0,0 +1,19 @@
+package com.basile.scala.ch02
+
+/**
+ * Write a function that computes x^n , where n is an integer.
+ * Use the following recursive definition:
+ * • x^n = y^2 if n is even and positive, where y = x^n / 2 .
+ * • x^n = x· x^n – 1 if n is odd and positive.
+ * • x^0 = 1.
+ * • x^n = 1 / x – n if n is negative.
+ *
+ * Don’t use a return statement.
+ */
+object Ex10 extends App {
+
+  def compute(x: Double, n: Int): Double = {
+    //1.2
+  }
+
+}
